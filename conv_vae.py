e99a2776bedaa253942066fc957535fdca0d768f
Clayton <cwseitz@indiana.edu>
2022-01-26 15:28:43 -0500

    update

--- conv_vae.py
+++ conv_vae.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
-from scipy.stats import kde
 import matplotlib.pyplot as plt
+import cv2
 from torchvision import datasets, transforms
+from scipy.stats import kde
 
 class ConvVAE(nn.Module):
     def __init__(self, latent_dim, hidden_dim=1024):
@@ -95,3 +96,30 @@ class ConvVAE(nn.Module):
 transform = transforms.Compose([transforms.ToTensor(), transforms.Resize(64), transforms.CenterCrop(64), transforms.Normalize((0.5,), (0.5,))])
 dataset = datasets.MNIST("data", download=True, transform=transform)
 loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+
+def show(x):
+    img = x.data.cpu().permute(1, 2, 0).numpy() * 255
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow(img)
+
+#training loop
+beta = 2
+for epoch in range(100):
+    for i, x in enumerate(loader):
+        if len(x) == 2:
+            x = x[0]
+        x = x.cuda()
+
+        mu, logvar, out = vae(x)
+        rl, kl, l = loss(x, out, mu, logvar, beta)
+
+        opt.zero_grad()
+        l.backward()
+        opt.step()
+
+        if i == 0:
+            vae.eval()
+            data = vae.generate(8)
+            grid_img = torchvision.utils.make_grid(data, nrow=8, normalize=True)
+            show(grid_img)
+            vae.train()
